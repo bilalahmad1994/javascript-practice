bf74ab150bb7528d661de69b05d781be25a5da1e
bilalahmad <bilalshakeel590@gmail.com>
2016-07-06 04:42:53 +0500

    first commit

---
+++ jstraining/js/ss.js
@@ -0,0 +1,146 @@
+/**
+ * Created by nehal nadeem on 6/22/2016.
+ */
+
+console.log("shakil  ahmad");
+console.log("bilal ahmad");
+/* var name=prompt("whats ur name");
+ alert("hi"  + name);
+ name="nick";
+ console.log("my name is  " + name);*/
+
+/*//if (name){
+//    console.log("success");}
+//    else {
+//       console.log("failure");
+//    }*/
+/*var name=prompt("what ur name ");*/
+
+/*var counter =10;
+while (counter){
+    console.log("hello world",counter);
+    counter=counter - 1;
+}
+while(prompt("what is ur name")){
+    console.log("got ur name ");
+}while */
+//
+/*
+console.log("after");
+for (var counter=10;counter;counter=counter-1){
+    console.log("hello world",counter);
+}*/
+
+//
+//var counter=10;
+//while (counter){
+//    console.log("personal",counter);
+//    counter=counter - 1;
+//}
+//var  succes=["ahmad","bilall","noman","nick"];
+//console.log(succes);
+//console.log(succes.length);
+//
+//var friends=1;
+//console.log(succes[friends]);
+//
+//for(var i=0;i<4;i+=1){
+//console.log(succes[i]);
+//}
+//var friends= ["a","b","c"];
+//console.log(friends);
+//
+//console.log(friends.length);
+//var solid=1;
+//console.log(friends[solid]);
+//
+//for (var i=0;i<3;i=i+1 ){
+//    console.log(friends[i]);
+//}
+//var counter=-10;
+//while (counter){
+//    console.log("helloworld",counter);
+//    counter=counter+2;
+//}
+//
+//var bb=["aa","bb","cc"];
+//
+//console.log(bb);
+//var thanks="thanks for ur input";
+//
+//var add=4;
+//var sum=7;
+//console.log(add+sum);
+////
+////var caseQty = 144;
+////alert(caseQty);
+////var name=34;
+////var bf=11;
+////name+bf;
+//
+//
+//var b=1;
+//while(counter);
+//console.log("");
+//var message = "Thanks, ";
+//var banger = "!";
+//var username="Susan";
+//alert("Thanks, " + "Susan" + "!");
+//
+
+//var message = "Thanks, ";
+//var userName = "Susan";
+//var banger = "!";
+//var customMess = message + userName + banger;
+//alert(customMess);
+
+//example
+//var ss="bilal";
+//var vv="ahmad";
+//var j=prompt(ss,vv);
+
+
+//vavr numberOfCats = prompt("How many cats?");
+//var tooManyCats = numberOfCats + 1
+//var cc="kk";
+//var bb=prompt("always happy man");
+//if(bb=="bilal"){//    alert("ok");
+//}
+//else {
+//    alert(cc);
+//}
+
+
+//var age     = 20;
+//var newAge  = ++age;
+//alert(newAge);
+//alert(age);
+
+//var count = 5;
+//var updatedCount = count + 1;
+//alert(updatedCount);
+//alert(count);
+//
+//
+//var age = "19";
+//age = age + 1;
+//alert(age);
+
+/*var num1 = prompt("Please enter first number?");
+num1 = parseInt(num1);
+var num2 = prompt("Please enter second number?");
+num2 = parseInt(num2);
+var sum = num1 + num2;
+document.write("Sum of two numbers is :" + sum);
+document.write("<br>");*/
+
+
+var num=prompt("first number");
+num=parseInt(num);
+var num1=prompt("second number");
+num1=parseInt(num1);
+var sum=num+num1;
+document.write("sum of two num is " +sum);
+document.write("<br>");
+
+var num
